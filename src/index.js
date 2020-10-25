4e3d3065e3d46560255fc72618e5f2a6be2d1848
Kokosik11 <62614032+Kokosik11@users.noreply.github.com>
2020-10-25 21:27:09 +0300

    feat: complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let prevStr;
+    while (str !== prevStr) {
+        prevStr = str;
+        bracketsConfig.map(([open, close]) => {
+            str = str.replace(`${open}${close}`, '');
+        });
+    }
+
+    return !str;
 }
